ad30ffc319b7af6b137d514d2602540ee1f5124a
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2026-07-04 22:45:40 +0300

    part 14: ex 15

--- part14/blog-list/app/actions/users.ts
+++ part14/blog-list/app/actions/users.ts
@@ -5,14 +5,54 @@ import { createNewUser } from "@/app/services/users"
 import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
 
-export const registerUser = async (formData: FormData) => {
+export interface RegisterUserState {
+  values: {
+    name: string
+    username: string
+  }
+  errors: {
+    username?: string
+    name?: string
+    passwordDidNotMatch?: string
+    usernameExists?: string
+  }
+}
+
+export const registerUser = async (
+  prevState: RegisterUserState,
+  formData: FormData
+) => {
+  const errors: RegisterUserState["errors"] = {};
+
   const name = formData.get("name") as string
+  if (!name || name.length < 4) {
+    errors.name = "Name must be at least 4 characters long"
+  }
+
   const username = formData.get("username") as string
+  if (!username || username.length < 4) {
+    errors.username = "Username must be at least 4 characters long"
+  }
+
   const password = formData.get("password") as string
+  const passwordConfirm = formData.get("passwordConfirm") as string
+  if (password !== passwordConfirm) {
+    errors.passwordDidNotMatch = "Passwords did not match"
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors, values: { name, username } }
+  }
 
   const passwordHash = await bcrypt.hash(password, 10)
 
-  await createNewUser(username, name, passwordHash)
+  // not the best error handling but good enough for this task
+  try {
+    await createNewUser(username, name, passwordHash)
+  } catch {
+    errors.usernameExists = "User with this username already exists"
+    return { errors, values: { name, username } }
+  }
 
   revalidatePath("/users")
   redirect("/login")
--- part14/blog-list/app/register/page.tsx
+++ part14/blog-list/app/register/page.tsx
@@ -1,28 +1,61 @@
-import { registerUser } from "@/app/actions/users";
+"use client"
+
+import { registerUser, RegisterUserState } from "@/app/actions/users";
+import { useActionState } from "react";
+
+const initialState: RegisterUserState = {
+  values: {
+    username: "",
+    name: "",
+  },
+  errors: {}
+}
 
 const RegisterPage = () => {
+  const [state, formAction] = useActionState(registerUser, initialState)
+
   return (
     <div>
       <h2>Register</h2>
-      <form action={registerUser}>
+      <form action={formAction}>
         <div>
           <label>
             Username
-            <input type="text" name="username" required />
+            <input
+              type="text"
+              name="username"
+              defaultValue={state.values.username}
+              required
+            />
           </label>
         </div>
+        {state.errors.username && <p style={{ color: "red" }}>{state.errors.username}</p>}
+        {state.errors.usernameExists && <p style={{ color: "red" }}>{state.errors.usernameExists}</p>}
         <div>
           <label>
             name
-            <input type="text" name="name" required />
+            <input
+              type="text"
+              name="name"
+              defaultValue={state.values.name}
+              required
+            />
           </label>
         </div>
+        {state.errors.name && <p style={{ color: "red" }}>{state.errors.name}</p>}
         <div>
           <label>
             Password
             <input type="password" name="password" required />
           </label>
         </div>
+        <div>
+          <label>
+            Password Confirmation
+            <input type="password" name="passwordConfirm" required />
+          </label>
+        </div>
+        {state.errors.passwordDidNotMatch && <p style={{ color: "red" }}>{state.errors.passwordDidNotMatch}</p>}
         <button type="submit">register</button>
       </form>
     </div>
